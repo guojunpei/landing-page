74f91eb277b6850fc3ad767846929a23c34792db
guojunpei <gjp599@163.com>
2021-07-11 23:30:52 +0800

    fix AnswerOption.ts

--- src/types/answerOption.ts
+++ src/types/answerOption.ts
@@ -1,15 +1,5 @@
-export interface AnswerOption {
-    a:	string;
-    b:	string;
-    c?:	string;
-    d?:	string;
-    e?:	string;
-    f?:	string;
-    g?:	string;
-    h?:	string;
-    i?:	string;
-    j?:	string;
-    k?:	string;
-    l?:	string;
-    m?:	string;
+import { OptionKey } from "../enums/optionKey";
+
+export type AnswerOption = {
+    [key in OptionKey]?:string
 }
