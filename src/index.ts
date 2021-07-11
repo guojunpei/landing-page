a702a0e568584879c043b558f8959a3300879ee1
guojunpei <gjp599@163.com>
2021-07-11 22:29:59 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,37 @@ import './styles/main.scss';
 
 import { OptionKey } from './enums/optionKey';
 import { QuestionType } from './enums/questionType';
+import { Question } from './types/question';
 
 
 const app = document.querySelector('#app');
+
+const questionList: Question[] = [
+  {
+    id:"Q1",
+    question:"Which of the following is not JavaScript Data Types?",
+    answerOption:{
+        A:"Undefined",
+        B:"Number",
+        C:"Boolean",
+        D:"Float",
+    },
+    rightAnswer:{
+        optionScore:{
+            right:10,
+            wrong:-5,
+            includeEquipartition:0,
+            min:0,
+            notInclude:0,
+        },
+        optionScoreRule:"答对 10分，答错 -5 分 扣分",
+        optionKey:[OptionKey.D],
+        optionValue:["Float"],
+    },
+    type:QuestionType.Radio,
+    score:0,
+}
+]
 
 const productList: Product[] = [
   {
--- src/types/question.ts
+++ src/types/question.ts
@@ -5,8 +5,32 @@ export interface Question {
   id: string;
   question: string;
   answerOption:{
-    OptionKey: string;
-
-      
-  }
+    A:	string;
+    B:	string;
+    C?:	string;
+    D?:	string;
+    E?:	string;
+    F?:	string;
+    G?:	string;
+    H?:	string;
+    I?:	string;
+    J?:	string;
+    K?:	string;
+    L?:	string;
+    M?:	string;
+  },
+  rightAnswer:{
+    optionScore:{
+        right: number,
+        wrong: number,
+        includeEquipartition: number,
+        min: number,
+        notInclude: number,
+    },
+    optionScoreRule: string,
+    optionKey: [OptionKey,OptionKey?,OptionKey?,OptionKey?,OptionKey?,OptionKey?],
+    optionValue: [string,string?,string?,string?,string?,string?],
+  },
+  type: QuestionType,
+  score: number,
 }
