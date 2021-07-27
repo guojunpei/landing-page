792ddb1f3e76d39046ef9eb201209c2234590bef
guojunpei <gjp599@163.com>
2021-07-28 00:29:14 +0800

    not done

--- src/types/answerSave.ts
+++ src/types/answerSave.ts
@@ -1,8 +1,12 @@
 import { OptionKey } from "../enums/optionKey";
 import { Question } from "../types/question";
 
+let question:Question;
+let questionId:string=question.id;
+let questionIds:[string]=[questionId];
+
 export type AnwserSave = {
     score:number;
     time:Date;
-    Question?:OptionKey
+    //[key in questionIds]?:OptionKey;
 }
