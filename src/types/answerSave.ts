5e318f77d8e8dbcbd4a342b38e135c0943272efa
guojunpei <gjp599@163.com>
2021-07-31 23:30:46 +0800

    save

--- src/types/answerSave.ts
+++ src/types/answerSave.ts
@@ -1,18 +1,23 @@
+/*
 import { OptionKey } from "../enums/optionKey";
 import { Question } from "../types/question";
 
 let question:Question;
 let questionId:string=question.id;
 let questionIds:string[]=[questionId];
+*/
 
 export type AnwserSave = {
     score:number;
     time:Date;
     //[key:string]:string;
     //[key in OptionKey]?:string
-    questionIds:string[];
-    answerContents:object[];
+    answerAndquestion:object[];
+    //**objct{questionid:string;answerContent:string;answerOption:optionKey;}
+    questionIds?:string[];
+    answerContents?:object[];
 }
-
+/*
 type Point = { x: number; y: number };
 type P = keyof Point;
+*/
